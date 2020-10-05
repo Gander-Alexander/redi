207660cc232be571d914a48977d5615c6a99ec16
Gander-Alexander <59661103+Gander-Alexander@users.noreply.github.com>
2020-10-05 00:05:12 -0600

    Added all files from the previous repo

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// import {loggedInButton} from '/index.js';
+
+// console.log(loggedInButton);
+// const submitButton = document.querySelector('.submit-button');
+
+// submitButton.addEventListener('click', function(e){
+//     const loggedInButton = document.querySelector('.log-in-button');
+//     loggedInButton.style.color = "blue";
+//     // this.classList.add('is-hidden');//adds is-hidden class
+//     // logout.classList.remove('is-hidden');//removes is-hidden class
+//   });
+
+
+
+
+let loggedIn = false;
+
+
+
+function login() {
+   let loggedIn = true;
+    return loggedIn
+
+}
+
+ if(loggedIn) {
+  console.log(loggedIn) 
+}
