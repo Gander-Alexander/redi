b81469b79f6bcf95c7f41b4b275ba471ec6bab03
Gander-Alexander <Gander-Alexander@users.noreply.github.com>
2020-10-09 16:19:48 -0600

    Messed around with javascript to try and create a post adding button

--- js/user-page-section/user-post.js
+++ js/user-page-section/user-post.js
@@ -1,1 +1,7 @@
 // This is for the user home page so that a user may create/send a post to the site's database
+
+function addingPost() {
+    var d = new Date();
+    var n = d.toString();
+    document.getElementById("demo").innerHTML = n;
+}
